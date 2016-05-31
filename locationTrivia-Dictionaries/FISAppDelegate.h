4df4354c7cc72d178cf02ed24abc61c5be5b0011
pressstart101 <walterscott404@yandex.com>
2016-05-31 16:05:35 -0400

    done

--- locationTrivia-Dictionaries/FISAppDelegate.h
+++ locationTrivia-Dictionaries/FISAppDelegate.h
@@ -6,9 +6,19 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
+ - (NSString *)stringByTruncatingNameOfLocation:(NSDictionary *)location
+                                       toLength:(NSUInteger)length;
+ - (NSDictionary *)dictionaryForLocationWithName:(NSString *)name
+                                      latitude:(CGFloat)latitude
+                                    longitude:(CGFloat)longitude;
+- (NSArray *)namesOfLocations:(NSArray *)locations;
+- (BOOL)dictionaryIsValidLocation:(NSDictionary *)dictionary;
+ - (NSDictionary *)locationNamed:(NSString *)name
+                     inLocations:(NSArray *)locations;
 /**
  
  *  Declare your method names here.
+ 
  
  */
 
--- locationTrivia-Dictionaries/FISAppDelegate.m
+++ locationTrivia-Dictionaries/FISAppDelegate.m
@@ -10,7 +10,75 @@
     return YES;
 }
 
-/** 
+
+
+
+
+
+
+- (NSString *)stringByTruncatingNameOfLocation:(NSDictionary *)location
+                                      toLength:(NSUInteger)length{
+    NSString *name = location[@"name"];
+    
+    if (name.length > length) {
+        return [name substringToIndex:length];
+    } else {
+        return name;
+    }
+}
+- (NSDictionary *)dictionaryForLocationWithName:(NSString *)name
+                                       latitude:(CGFloat)latitude
+                                     longitude:(CGFloat)longitude{
+    
+    NSNumber *latitudeNumber = @(latitude);
+    NSNumber *longitudeNumber = @(longitude);
+    
+    NSMutableDictionary *resultDictionary = [@{@"name" : name,
+                                               @"latitude" : latitudeNumber,
+                                               @"longitude" : longitudeNumber}
+                                             mutableCopy];
+    return resultDictionary;
+}
+- (NSArray *)namesOfLocations:(NSArray *)locations{
+    NSMutableArray *resultArray = [[NSMutableArray alloc]init];
+    
+    for (NSInteger i = 0; i< [locations count]; i++){
+        
+        if ([[locations[i]allKeys]containsObject:@"name"]){
+            NSString *str = locations[i][@"name"];
+            [resultArray addObject:str];
+
+            NSLog(@"\n\n\n\n%@\n\n\n\n", str);
+        }else{
+            return resultArray;
+        }
+    }
+    return resultArray;
+}
+- (BOOL)dictionaryIsValidLocation:(NSDictionary *)dictionary{
+    NSMutableSet *resultSet = [[NSMutableSet alloc]init];
+    NSSet *compareTo = [NSSet setWithObjects:@"name", @"latitude", @"longitude", nil];
+    for(NSString *key in dictionary){
+        [resultSet addObject:key];
+    }
+    if ([resultSet isEqual:compareTo]){
+        return YES;
+    }else{
+        return NO;
+    }
+}
+- (NSDictionary *)locationNamed:(NSString *)name
+                    inLocations:(NSArray *)locations{
+    for (NSDictionary *element in locations){
+        if ([element[@"name"] isEqualToString: name]){
+            return element;
+        }
+    
+    }
+return nil;
+}
+
+/**
  
  *  Write your method implementations here.
  
